848cdaacb5f3f61c4eb486dd3da4156c91262454
onrush1995 <moniruzzaman.eng1995@gmail.com>
2019-01-16 22:13:07 +0200

    Student data with routers

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
 
-//Require the Router we defined in movies.js
+//Require the Router we defined in students.js
 var students = require("./students.js");
 
-//Use the Router on the sub route /movies
+//Use the Router on the sub route /students
 app.use("/students", students);
 
 app.listen(5000);
--- students.js
+++ students.js
@@ -26,6 +26,108 @@ var students = [
     Address: "Kajaanintie 08,90130,OULU"
   }
 ];
+router.get("/", function(req, res) {
+  res.json(students);
+});
+router.get("/:id([0-9]{3,})", function(req, res) {
+  res.json(students);
+  var currStudent = students.filter(function(student) {
+    if (student.id == req.params.id) {
+      return true;
+    }
+  });
+  if (currStudent.length == 1) {
+    res.json(currStudent[0]);
+  } else {
+    res.status(404); //Set status to 404 as movie was not found
+    res.json({ message: "Not Found" });
+  }
+});
+
+router.post("/", function(req, res) {
+  //Check if all fields are provided and are valid:
+
+  var newId = students[students.length - 1].id + 1;
+  students.push({
+    id: newId,
+    name: req.body.name,
+    class: req.body.class,
+    Address: req.body.Address
+  });
+  res.json({
+    message: "New student created.",
+    location: "/students/" + newId
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var updateIndex = students
+    .map(function(student) {
+      return student.id;
+    })
+    .indexOf(parseInt(req.params.id));
+
+  if (updateIndex === -1) {
+    //student not found, create new
+    students.push({
+      id: req.params.id,
+      name: req.body.name,
+      class: req.body.year,
+      Address: req.body.rating
+    });
+    res.json({
+      message: "New student created.",
+      location: "/students/" + req.params.id
+    });
+  } else {
+    //Update existing student
+    students[updateIndex] = {
+      id: req.params.id,
+      name: req.body.name,
+      class: req.body.year,
+      Address: req.body.rating
+    };
+    res.json({
+      message: "Student id " + req.params.id + " updated.",
+      location: "/students/" + req.params.id
+    });
+  }
+});
+
+router.put("/:id", function(req, res) {
+  var updateIndex = students
+    .map(function(student) {
+      return student.id;
+    })
+    .indexOf(parseInt(req.params.id));
+
+  if (updateIndex === -1) {
+    //student not found, create new
+    students.push({
+      id: req.params.id,
+      name: req.body.name,
+      class: req.body.year,
+      Address: req.body.rating
+    });
+    res.json({
+      message: "New student created.",
+      location: "/students/" + req.params.id
+    });
+  } else {
+    //Update existing student
+    students[updateIndex] = {
+      id: req.params.id,
+      name: req.body.name,
+      class: req.body.year,
+      Address: req.body.rating
+    };
+    res.json({
+      message: "Student id " + req.params.id + " updated.",
+      location: "/students/" + req.params.id
+    });
+  }
+});
+
 router.delete("/:id", function(req, res) {
   var removeIndex = students
     .map(function(student) {
